1a832f86632b1738455207de069692991ad16d48
gb10udn <rbbpx0926@gmail.com>
2024-02-04 15:18:30 +0900

    config.ini を管理対象から外した

--- bluetooth_connection.py
+++ bluetooth_connection.py
@@ -1,4 +1,5 @@
 import subprocess
+import configparser
 import questionary
 
 # subprocess.Popen("ping")
@@ -19,8 +20,21 @@ def disconnect(device_name: str):
     subprocess.run(cmd)
 
 
+def read_device_name_from_config(*, path='config.ini') -> str:
+    """
+    コンフィグファイルから
+    """
+    try:
+        config_ini = configparser.ConfigParser()
+        config_ini.read(path, encoding='utf-8')  # FIXME: 240129 コンフィグのパスがない場合のハンドリングを記述する。(他にも参照している箇所があった気がする。)
+        return config_ini['GENERAL']['device_name']
+    
+    except:
+        return None
+
+
 if __name__ == '__main__':
-    command = questionary.select(
+    action = questionary.select(
         'Select bluetooth action',
-        choices=['connect', 'disconnect'],
+        choices=['connect', 'disconnect'],  # TODO: 240204 現状のステータスを呼べるようにしてもいいかも？
     ).ask()
